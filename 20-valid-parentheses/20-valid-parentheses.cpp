00453ce837336abbffe428f664b3dde9df7384e8
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-01-05 16:49:53 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (78.78%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '(' or s[i]=='[' or s[i]=='{') st.push(s[i]);
+            
+            else if(s[i]==')'){
+                if(st.empty()) return false;
+                if(st.top()=='(') st.pop();
+                else return false;
+            }
+            else if(s[i]==']'){
+                if(st.empty()) return false;
+                if(st.top()=='[') st.pop();
+                else return false;
+            }
+            else if(s[i]=='}'){
+                if(st.empty()) return false;
+                if(st.top()=='{') st.pop();
+                else return false;
+            }
+        }
+        if(st.empty()) return true;
+        return false;
+    }
+};
