30f9fdcdfa8cf6b99d083783ab5a196d02151880
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-01-07 19:09:00 +0530

    Time: 121 ms (26.04%), Space: 48.4 MB (48.14%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        int i=0;
+        
+        for(int reach=0; i<n and i<=reach; i++){
+            reach = max(i+nums[i], reach);
+        }
+        return i==n;
+    }
+};
