006fcb8e8a1a52432745d7e045b8527d04085a4d
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-02-07 10:41:35 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (67.00%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        if(s.size() == 0) return t[0];
+        char  ans = 0;
         
-        unordered_map<char, int> mp;
+        for(auto it : s) ans ^= it;
+        for(auto it : t) ans ^= it;
         
-        for(int i=0; i<s.size(); i++) mp[s[i]]++;
-        
-        for(int i=0; i<t.size(); i++){
-            mp[t[i]]--;
-        }
-        
-        for(auto it : mp){
-            if(it.second != 0) return it.first;
-        }
-        return '*';
+        return ans;
     }
 };
