af0c5b445c3914717dfca7c61d50458f62c41012
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-02-16 12:08:57 +0530

    Time: 4 ms (57.52%), Space: 7.7 MB (7.38%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -10,33 +10,28 @@
  */
 class Solution {
 public:
-    ListNode *reverseK(ListNode *&head, int k)
-{
-    if(!head) return nullptr;
-    if(!head->next) return head;
-    ListNode *prevptr = NULL;
-    ListNode* currptr = head;
-    ListNode *nextptr;
-    int count = 0;
+    ListNode *reverseK(ListNode *&head, int k) {
+        
+        if(!head || !head->next) return head;
+        
+        ListNode *prevptr = NULL, *currptr = head, *nextptr;
+        int count = 0;
 
-    while (currptr != NULL && count < k)
-    {
-        nextptr = currptr->next;
-        currptr->next = prevptr;
-        prevptr = currptr;
-        currptr = nextptr;
-        count++;
+        while (currptr != NULL && count < k) {
+            nextptr = currptr->next;
+            currptr->next = prevptr;
+            prevptr = currptr;
+            currptr = nextptr;
+            count++;
+        }
+
+        if (nextptr != NULL) {
+            head->next = reverseK(nextptr, k);
+        }
+
+        return prevptr;
     }
-
-    if (nextptr != NULL)
-    {
-        head->next = reverseK(nextptr, k);
-    }
-
-    return prevptr;
-}
     ListNode* swapPairs(ListNode* head) {
-        if(!head) return nullptr;
         return reverseK(head, 2);
     }
 };
