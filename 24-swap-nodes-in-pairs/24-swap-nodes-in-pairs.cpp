869b176a9f41a7711362e6e5fad3b2e450477b22
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-02-16 12:11:16 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (55.41%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -1,37 +1,28 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode *reverseK(ListNode *&head, int k) {
+    ListNode *reverseK(ListNode *&head, int k)
+{
+    if(!head) return nullptr;
+    if(!head->next) return head;
         
-        if(!head) return nullptr;
-        if(!head->next)  return head;
-        
-        ListNode *prevptr = NULL, *currptr = head, *nextptr;
-        int count = 0;
+    ListNode *prevptr = NULL;
+    ListNode* currptr = head;
+    ListNode *nextptr;
+    int count = 0;
 
-        while (currptr != NULL && count < k) {
-            nextptr = currptr->next;
-            currptr->next = prevptr;
-            prevptr = currptr;
-            currptr = nextptr;
-            count++;
-        }
-
-        if (nextptr != NULL) {
-            head->next = reverseK(nextptr, k);
-        }
-
-        return prevptr;
+    while (currptr != NULL && count < k) {
+        nextptr = currptr->next;
+        currptr->next = prevptr;
+        prevptr = currptr;
+        currptr = nextptr;
+        count++;
     }
+
+    if (nextptr != NULL)
+        head->next = reverseK(nextptr, k);
+
+    return prevptr;
+}
     ListNode* swapPairs(ListNode* head) {
         if(!head) return nullptr;
         return reverseK(head, 2);
