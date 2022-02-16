d8335d089267068e8623191fe1e4662966946103
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-02-16 12:06:44 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (55.41%) - LeetHub

---
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *reverseK(ListNode *&head, int k)
+{
+    if(!head) return nullptr;
+    if(!head->next) return head;
+    ListNode *prevptr = NULL;
+    ListNode* currptr = head;
+    ListNode *nextptr;
+    int count = 0;
+
+    while (currptr != NULL && count < k)
+    {
+        nextptr = currptr->next;
+        currptr->next = prevptr;
+        prevptr = currptr;
+        currptr = nextptr;
+        count++;
+    }
+
+    if (nextptr != NULL)
+    {
+        head->next = reverseK(nextptr, k);
+    }
+
+    return prevptr;
+}
+    ListNode* swapPairs(ListNode* head) {
+        if(!head) return nullptr;
+        return reverseK(head, 2);
+    }
+};
