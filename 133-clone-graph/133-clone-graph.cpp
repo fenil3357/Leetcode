9de44eac3471f4c8d58c2d6064c8176c2c23ab57
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-02-24 00:59:46 +0530

    Time: 4 ms (93.09%), Space: 8.9 MB (45.65%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.cpp
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+    
+    private:
+    unordered_map<int, Node*> mp;
+public:
+    
+    Node* helper(Node* node){
+        if(mp.find(node->val) != mp.end()) return mp[node->val];
+        
+        Node* newNode = new Node(node->val);
+        mp[node->val] = newNode;
+        
+        for(auto x : node->neighbors){
+            newNode->neighbors.push_back(helper(x));
+        }        
+        return newNode;
+    }
+    Node* cloneGraph(Node* node) {
+        if(!node) return  nullptr;
+        return helper(node);
+    }
+};
