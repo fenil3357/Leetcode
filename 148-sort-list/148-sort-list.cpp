df7f484ad86c7cdf0e7cd4b9f7bba4ba90f003d9
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-02-24 16:36:21 +0530

    Time: 84 ms (93.99%), Space: 30.3 MB (47.63%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -10,23 +10,31 @@
  */
 class Solution {
 public:
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        if(!l1) return l2;
+        if(!l2) return l1;
+        
+        if(l1->val < l2->val) {
+            l1->next = merge(l1->next, l2);
+            return l1;
+        }
+        else{
+            l2->next = merge(l1, l2->next);
+            return l2;
+        }
+    }
     ListNode* sortList(ListNode* head) {
-        if(!head) return nullptr;
+        if(!head or !head->next) return head;
         
-        vector<int> v;
-        ListNode* temp = head;
-        while(temp) {
-            v.push_back(temp->val);
-            temp = temp->next;
+        ListNode* slow = head, *fast = head->next;
+        
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
         }
         
-        sort(v.begin(), v.end());
-        temp = head;
-        
-        for(auto it : v) {
-            temp->val = it;
-            temp = temp->next;
-        }
-        return head;
+        ListNode* newHead = slow->next;
+        slow->next = nullptr;
+        return merge(sortList(head), sortList(newHead));
     }
 };
