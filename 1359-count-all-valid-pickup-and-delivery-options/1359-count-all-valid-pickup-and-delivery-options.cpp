8e5d54f87bcf93134cf420d6abb5df712a65f68b
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-03-06 17:44:07 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (96.86%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long mod = 1e9+7; 
+    
+    int countOrders(int n) {
+        long long ans = 1;
+        
+        for(int i=1; i<=n; i++) {
+            ans = (ans*i)%mod;
+            ans = (ans * (2*i-1))%mod;
+        }
+        return ans;
+    }
+};
