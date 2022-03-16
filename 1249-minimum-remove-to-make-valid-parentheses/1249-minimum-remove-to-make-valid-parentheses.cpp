b8425090db9f2c39735a5f913f31dbf4d15cd14f
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-03-16 16:18:12 +0530

    Time: 30 ms (57.47%), Space: 11.5 MB (53.91%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        if(s.empty()) return "";
-
-        stack<char> st;
+        
+        stack<int> st;
         string ans = "";
 
         for(int i=0; i<s.size(); i++) {
-            if(s[i]>='a' && s[i]<='z') {
-                ans.push_back(s[i]);
-                continue;
-            }
-
-            if(s[i] == '(') {
-                ans.push_back('(');
-                st.push('(');
-            }
+            if(s[i] == '(') st.push(i);
 
             else if(s[i] == ')') {
-                if(!st.empty() && st.top() == '(') {
-                    ans.push_back(')');
-                    st.pop();
-                }
+                if(st.empty()) s[i] = '#';
+                else st.pop();
             }
         }
 
-        while(!st.empty()) st.pop();
-
-        for(int i=ans.size()-1; i>=0; i--) {
-            if(ans[i] == ')') st.push(')');
-            else if(ans[i] == '(') {
-                if(!st.empty() && st.top()==')') {
-                    st.pop();
-                }
-                else ans.erase(i, 1);
-            }
+        while(!st.empty()) {
+            s[st.top()] = '#';
+            st.pop();
         }
+
+        for(int i=0; i<s.size(); i++) {
+            if(s[i] != '#') ans.push_back(s[i]);
+        }
+        
         return ans;
     }
 };
