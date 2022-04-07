1ca5b8d1a5ee98332e74863611c010959e360763
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-04-07 19:16:22 +0530

    Time: 184 ms (91.30%), Space: 92 MB (49.13%) - LeetHub

--- 2227-encrypt-and-decrypt-strings/2227-encrypt-and-decrypt-strings.cpp
+++ 2227-encrypt-and-decrypt-strings/2227-encrypt-and-decrypt-strings.cpp
@@ -1,30 +1,40 @@
 class Encrypter {
 public:
-    unordered_map<char, string> mp;
-    unordered_map<string, int> m;
-    Encrypter(vector<char>& keys, vector<string>& values, vector<string>& dict) {
-        for(int i=0; i<keys.size();i++) {
-            mp[keys[i]] = values[i];
-        }
 
-        for(auto it : dict) m[encrypt(it)]++;
+	// To map all the keys with their values
+    unordered_map<char, string> key_value_map;
+	
+	// To keep counts of all encrypted values of strings from dict
+    unordered_map<string, int> dict_map;
+	
+	
+    Encrypter(vector<char>& keys, vector<string>& values, vector<string>& dict) {
+	
+		// Mapping all keys to their values
+        for(int i=0; i<keys.size();i++)  key_value_map[keys[i]] = values[i];
+
+		// Counting the number of encrypted strings from dict
+        for(auto it : dict) dict_map[encrypt(it)]++;
     }
     
     string encrypt(string word1) {
         string ans;
 
         for(int i=0; i<word1.size(); i++) {
-            if(mp.find(word1[i]) == mp.end()) {
+
+            // If we found a character not present in key we will add '#' to ans
+            if(key_value_map.find(word1[i]) == key_value_map.end()) {
                 ans += '#';
             }
             else
-                ans += mp[word1[i]];
+                ans += key_value_map[word1[i]];
         }
 
         return ans;
     }
     
     int decrypt(string word2) {
-        return m[word2];
+        // Returning the count of encrypted string from dict matching with word2
+        return dict_map[word2];
     }
 };
