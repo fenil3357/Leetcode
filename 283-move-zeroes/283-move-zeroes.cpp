9692250a356f34949472d25ca36ec1d5881322f6
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-05-02 12:12:52 +0530

    Time: 34 ms (44.26%), Space: 19.3 MB (14.33%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        for(int i=0, j=0; j<nums.size(); j++) {
+        for(int i=0, j=0; j<nums.size(); j++)
             if(nums[j]!=0) swap(nums[i++], nums[j]);
-        }
     }
 };
