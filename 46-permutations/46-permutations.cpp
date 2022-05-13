a04c48039cc65bb815fde9b369b04833a094fb51
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-05-13 09:48:10 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (98.54%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+
+        vector<vector<int> > res;
+
+        res.push_back(nums);
+
+        while(next_permutation(begin(nums), end(nums))) {
+            res.push_back(nums);
+        }
+
+        return res;
+    }
+};
