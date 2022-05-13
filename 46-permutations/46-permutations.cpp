831ccc77edc81902e2f0f6875977aac5faf761d9
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-05-13 11:53:09 +0530

    Time: 8 ms (25.34%), Space: 7.7 MB (78.48%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        sort(begin(nums), end(nums));
-
-        vector<vector<int> > res;
-
-        res.push_back(nums);
-
-        while(next_permutation(begin(nums), end(nums))) {
-            res.push_back(nums);
+    void solve(vector<int> &curr, vector<vector<int> > &res, unordered_map<int, int> &mp, int n) {
+        if(curr.size() == n) {
+            res.push_back(curr);
+            return;
         }
 
+        for(auto it : mp) {
+            if(it.second == 0) continue;
+
+            curr.push_back(it.first);
+            mp[it.first]--;
+
+            solve(curr, res, mp, n);
+
+            curr.pop_back();
+            mp[it.first]++;
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int> > res;
+        vector<int> curr;
+        int n = nums.size();
+        unordered_map<int, int> mp;
+
+        for(auto it : nums) mp[it]++;
+
+        solve(curr, res, mp, n);
         return res;
     }
 };
