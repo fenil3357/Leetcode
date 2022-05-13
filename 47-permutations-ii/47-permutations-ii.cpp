93dcd9dcd4157e159d480cf900db8fa5b99ccd1a
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-05-13 10:38:47 +0530

    Time: 9 ms (67.34%), Space: 8.4 MB (89.32%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,14 +1,37 @@
 class Solution {
 public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        sort(begin(nums), end(nums));
 
-        vector<vector<int> > res;
-        res.push_back(nums);
+    void permutation(vector<int> &v) {
+        if(v.size() <= 1) return;
 
-        while(next_permutation(begin(nums), end(nums))) {
-            res.push_back(nums);
+        int pos = 0;
+
+        for(int i=v.size()-2; i>=0; i--) {
+            if(v[i] < v[i+1]) {
+                pos = i;
+                break;
+            }
         }
+
+        for(int i=v.size()-1; i>=pos; i--) {
+            if(v[pos] < v[i]) {
+                swap(v[i], v[pos]);
+                pos++;
+                break;
+            }
+        }
+
+        reverse(begin(v) + pos, end(v));
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int> > res;
+        vector<int> initial(nums);
+        do {
+            res.push_back(nums);
+            permutation(nums);
+        }while(nums != initial);
+
         return res;
     }
 };
