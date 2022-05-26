2b7d58680def5afb1d5ecd7260e351f3b2085c19
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-05-26 10:48:46 +0530

    Time: 0 ms (100.00%), Space: 6 MB (48.74%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int cnt = 0;
-        
-        while(n != 0){
-            n -= (n & ~n+1);
-            cnt++;
-        }
-        return cnt;
+        return __builtin_popcountll(n);
     }
 };
