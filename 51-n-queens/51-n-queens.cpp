f4bcc15a802ec9ce7c08d2ebbc37514bb160c4d7
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-05-28 12:16:11 +0530

    Time: 3 ms (96.13%), Space: 7.3 MB (68.00%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isValid(vector<string> &nQ, int row, int col, int &n) {
+        for(int i=0; i != row; i++) 
+            if(nQ[i][col] == 'Q')
+                return false;
+
+        for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--)
+            if(nQ[i][j] == 'Q')
+                return false;
+
+        for(int i=row-1, j=col+1; i>=0 && j<n; i--, j++) 
+            if(nQ[i][j] == 'Q')
+                return false;
+
+        return true;
+    }
+
+    void solve(vector<vector<string>> &res, vector<string> &nQ, int row, int &n) {
+        if(row == n) {
+            res.push_back(nQ);
+            return;
+        }
+
+        for(int col = 0; col != n; col++) {
+            if(isValid(nQ, row, col, n)) {
+                nQ[row][col] = 'Q';
+                solve(res, nQ, row+1, n);
+                nQ[row][col] = '.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string> > res;
+        vector<string> nQueens(n, string(n, '.'));
+        solve(res, nQueens, 0, n);
+        return res;
+    }
+};
