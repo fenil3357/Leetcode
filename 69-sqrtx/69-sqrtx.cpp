761f0150170060c005621d021c8b5660ffd249e0
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-08-08 19:04:45 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (25.28%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        long long l = 0, e = x, ans, mid;
+        
+        while(l <= e) {
+            mid = (l + e)/2;
+            if(mid*mid == x) return mid;
+            else if(mid*mid < x) {
+                ans = mid;
+                l = mid + 1;
+            }
+            else e = mid - 1;
+        } 
+        return ans;
     }
 };
