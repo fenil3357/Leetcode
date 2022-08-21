474e5a728d53a3b38b2e5af0d9304a99c475eaef
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-08-21 10:49:41 +0530

    Time: 37 ms (100.00%), Space: 13.7 MB (100.00%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.cpp
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     string largestPalindromic(string num) {
-        if(num.empty()) return num;
-
         string ans1, ans2;
         vector<int> cnt(10);
         for(auto &i : num) cnt[i - '0']++;
 
         for(int i=9; i>=0; i--) {
-            if(i==0 && ans1.empty()) break;
-
+            if(i==0 && ans1.empty()) break; // To avoid leading zeroes
             int tmp = cnt[i]/2;
             for(int j=0; j<tmp; j++) ans1.push_back(i + '0');
             cnt[i] -= 2*tmp;
         }
-
         ans2 = ans1;
         reverse(ans2.begin(), ans2.end());
         
@@ -24,7 +20,6 @@ public:
                 break;
             }
         }
-
         return (ans1 + ans2);
     }
 };
