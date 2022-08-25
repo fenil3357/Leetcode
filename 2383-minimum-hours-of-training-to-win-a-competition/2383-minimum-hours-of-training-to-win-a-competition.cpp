f8269f8715c3c7778df19a28f50eaf6d2e5bee31
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-08-25 20:55:55 +0530

    Time: 7 ms (75.00%), Space: 11.3 MB (8.33%) - LeetHub

---
+++ 2383-minimum-hours-of-training-to-win-a-competition/2383-minimum-hours-of-training-to-win-a-competition.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minNumberOfHours(int ie, int ig, vector<int>& energy, vector<int>& experience) 
+    {
+        int hours = 0;
+        for (int i = 0; i < energy.size(); i++) {
+            if (energy[i] >= ie) {
+                hours += energy[i] - ie + 1;
+                ie += energy[i] - ie + 1;
+            }
+            if (experience[i] >= ig) {
+                hours += experience[i] - ig + 1;
+                ig += experience[i] - ig + 1;
+            }
+            ie -= energy[i];
+            ig += experience[i];
+        }
+        return hours;
+    }
+};
