a6907d9ee673a5b6c25fe709a94534eaabeca746
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-08-26 10:55:17 +0530

    Time: 22 ms (62.95%), Space: 8.7 MB (72.69%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canConstruct(string s, string t) {
+        vector<int> v(26, 0);
+        
+        for(auto &it : s) v[it - 'a']++;
+        for(auto &it : t) v[it - 'a']--;
+        
+        for(auto &it : v) if(it>0) return false;
+        return true;
+    }
+};
