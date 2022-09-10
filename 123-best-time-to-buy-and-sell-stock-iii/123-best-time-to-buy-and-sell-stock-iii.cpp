bc032b8cfc8502288bf18f769f305abfa442d350
Fenil Rami <78013068+fenil3357@users.noreply.github.com>
2022-09-10 23:31:33 +0530

    Time: 570 ms (45.60%), Space: 115.7 MB (48.97%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,21 +1,31 @@
 class Solution {
+   /*   
+        pos->current position
+        t->transactions done
+        bought->If current stock is bought
+    */
+    vector<vector<vector<int>>> mem;
+    int recursion(vector<int>& prices,int pos,int t,bool bought)
+    {
+        if(pos>=prices.size() || t==0)      //Out of bounds case
+            return 0;
+        if(mem[bought][t][pos]!=-1)     //Return if already calculated
+            return mem[bought][t][pos];
+        
+        //3 choices for a position-->Buy/Sell/Skip
+        int result = recursion(prices,pos+1,t,bought);    //SKIP
+        if(bought)
+            result = max(result,recursion(prices,pos+1,t-1,false)+prices[pos]);   //SELL
+        else
+            result = max(result,recursion(prices,pos+1,t,true)-prices[pos]);    //BUY
+        
+        mem[bought][t][pos] = result;
+        return result;
+    }
 public:
     int maxProfit(vector<int>& prices) {
-        if(prices.size() <= 1) return 0;
-        
-        int k = 2, mxProf = 0;
-        
-        vector<vector<int>> dp(k+1, vector<int>(prices.size(), 0));
-        
-        for(int i=1; i<=k; i++) {
-            int tmpMx = dp[i-1][0] - prices[0];
-            
-            for(int j=1; j<prices.size(); j++) {
-                dp[i][j] = max(dp[i][j-1], prices[j] + tmpMx);
-                tmpMx = max(tmpMx, dp[i-1][j] - prices[j]);
-                mxProf = max(dp[i][j], mxProf);
-            }
-        }
-        return mxProf;
+        mem.resize(2,vector<vector<int>>(3,vector<int>(prices.size(),-1)));
+        int res = recursion(prices,0,2,false);
+        return res;
     }
 };
